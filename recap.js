81875963632837d84cb5677b091c29a7cd6d7920
noman-swe <noman.swe130@gmail.com>
2022-01-30 16:24:42 +0600

    recap of var, array,  condition, loop

---
+++ recap.js
@@ -0,0 +1,27 @@
+var favoriteBook = "4 hours work weak";
+
+//array
+var bookList = ['positioning', 'hooked', 'start with why', 'shoe dog'];
+var shoeDogIndex = bookList.indexOf('shoe dog');
+bookList[1] = 'Story Brand';
+
+bookList.push('Giants');
+bookList.pop();
+
+//conditionals
+if(bookList[1] = 'hooked'){
+    console.log('i am hooked');
+}else{
+    console.log('i am not hooked');
+}
+
+//while loop
+var i = 0;
+while( i < 10){
+    console.log('number ' + i);
+    i++;
+}
+
+for (var i = 0; i < 10; i++ ){
+    console.log('row :- ' + i);
+}
