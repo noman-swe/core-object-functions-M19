be5f2512e1ade0fc163441ed5af7855a7e856d3b
noman-swe <noman.swe130@gmail.com>
2022-01-31 01:15:04 +0600

    function.js

---
+++ function.js
@@ -0,0 +1,18 @@
+function turnOnFan(){
+ //console.log('walk towards the switch');
+ //console.log('press the switch');
+ console.log('fan er switch de');
+}
+//turnOnFan();
+
+function turnOffFan(){
+    console.log('off the fan');
+}
+//turnOffFan();
+
+console.log('cha khai');
+console.log('vaat khai');
+console.log('shui');
+turnOnFan();
+console.log('thanda lagtesy');
+turnOffFan();
