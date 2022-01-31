a0b726a706c7923445c2b8a3a5f3717982b06d53
noman-swe <noman.swe130@gmail.com>
2022-02-01 02:21:08 +0600

    Module:19-object added

---
+++ object.js
@@ -0,0 +1,22 @@
+var student1 = {
+    id: 115, 
+    name:'Shibly', 
+    marks: 81, 
+    phone: 234831465, 
+    height: '5.5' 
+};
+
+var mobile = {
+    color: 'balck',
+    price: 12000,
+    screenSize: 5,
+    storage: '16gb'
+};
+
+var book = {
+    title: 'Start with why',
+    author: 'Simon Sinek',
+    price: 200,
+    pages: 244,
+    publisher: 'penguin'
+};
