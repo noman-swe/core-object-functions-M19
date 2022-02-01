05972ae5a38b3928d04f5ccbd9f6b0deaaa7307d
noman-swe <noman.swe130@gmail.com>
2022-02-01 15:24:22 +0600

    shitch case

---
+++ switch-case.js
@@ -0,0 +1,50 @@
+var color = 'yellow';
+
+if (color == 'blue') {
+    console.log('color is : Blue');
+}
+else if (color == 'red') {
+    console.log('color is red');
+}
+else if (color == 'green') {
+    console.log('color is green');
+}
+else if (color == 'white') {
+    console.log('color is white');
+}
+else if (color == 'yellow') {
+    console.log('color is yellow');
+}
+else {
+    console.log('color is blsck');
+}
+
+
+
+//switch case.
+switch (color) {
+    case 'blue':
+        console.log("color is blue");
+        break;
+
+    case 'red':
+        console.log('red');
+        break;
+
+    case 'green':
+        console.log('it is green');
+        break;
+
+    case 'white':
+        console.log('it is white');
+        break;
+
+        case 'yellow':
+            console.log('it is yellow');
+            break;
+
+            default:
+                console.log('black');
+
+
+}
